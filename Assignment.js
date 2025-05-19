a6d706f64d904c99cae665b0f206cacd502f022f
unknown <olaomodipo77@gmail.com>
2025-05-19 16:39:58 +0100

    assignment.js

--- Assignment.js
+++ Assignment.js
@@ -3,3 +3,4 @@ const position = 2
 let output = "Today is " + day + ". It is the " + position+"nd day of the week."
 
 console.log(output)
+
--- task1.js
+++ task1.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 const day = "Tuesday"
 const position = 2
 let output = "Today is " + day + ". It is the " + position+"nd day of the week."
 
 console.log(output)
-=======
-
->>>>>>> 12d5ef8f4cf84c31556b6f3454c698509eb95533
