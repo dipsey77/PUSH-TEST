d412c347e7c5b933bef5081fba11db2b509bd930
dipsey77 <168734108+dipsey77@users.noreply.github.com>
2025-05-22 22:06:20 +0100

    task3.js

--- task3.js
+++ task3.js
@@ -1,8 +1,8 @@
-let side1 = 11
-let side2 = 11
-let side3 = 11
+let side1 = 10
+let side2 = 1
+let side3 = 15
 
-console.log("Below is the length of side of a triangle, I will determine the type of triangle.")
+console.log("Below is the length of sides of a triangle, I will determine the type of triangle.")
 console.log()
 
 console.log("Side one = " + side1)
@@ -10,12 +10,12 @@ console.log("Side two = " + side2)
 console.log("Side three = " + side3)
 console.log()
 
-if(side1 === side2 && side1 ===3){
-  console.log("This is a right angle triangle.")
+if(side1 === side2 && side1 ===side3){
+  console.log("This is an Equilateral triangle.")
 } 
-else if (side1 === side2 || side1 === side3 || side2 === side3){
-  console.log("This is a right angle triangle.")
+else if (side1 === side2 && side1 != side3 || side1 === side3 && side1 != side2 || side2 === side3 && side2 != side1){
+  console.log("This is an Isosceles triangle.")
 }
 else{
-  console.log("This is a strange triangle")
+  console.log("This is a scalene triangle")
 }
