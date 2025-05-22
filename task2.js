ce370f566904addc4e2096b694b8ac370bba7b7e
dipsey77 <168734108+dipsey77@users.noreply.github.com>
2025-05-22 16:34:21 +0100

    task2.js

--- task2.js
+++ task2.js
@@ -1,6 +1,10 @@
-const day = "Tuesday"
-const position = 2
-let output = "Today is " + day + ". It is the " + position+"nd day of the week."
+const number = 52
+console.log("I have a number in mind, is the number an even or odd number? The number is " + number)
 
-console.log(output)
+if (number % 2 === 0){
+    console.log("Ok! Got it, " + number + " is an even number")
+}
+else{
+    console.log("Ok! got it, " + number + " is an odd number")
+}
 
